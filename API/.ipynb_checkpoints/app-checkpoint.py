95c103dea91bf99ce07d02a39b15d74833ab0dd2
hado0123 <hado_123@naver.com>
2025-08-07 17:40:04 +0900

    up

--- API/.ipynb_checkpoints/app-checkpoint.py
+++ API/.ipynb_checkpoints/app-checkpoint.py
@@ -5,17 +5,44 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from scipy.sparse import coo_matrix, csr_matrix
 from implicit.als import AlternatingLeastSquares
+from sqlalchemy import create_engine
+
+# DB 연결 정보
+DB_USER = 'root'
+DB_PASSWORD = '1234'
+DB_HOST = '127.0.0.1'  # 또는 EC2 퍼블릭 IP
+DB_PORT = '3306'
+DB_NAME = 'shopmax'
+
+# SQLAlchemy로 MySQL 연결
+engine = create_engine(f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+
+# 쿼리 실행해서 pandas로 불러오기
+query = """
+SELECT 
+    o.userId AS user_id,
+    oi.itemId AS item_id,
+    CAST(SUM(oi.count) AS UNSIGNED) AS purchase_count
+FROM orders o, orderitems oi
+where o.id = oi.orderId
+group by oi.itemId, o.userId
+order by o.userId, oi.itemId;
+"""
+data = pd.read_sql(query, engine)
+
+print(data.head())
+
 
 app = FastAPI()
 
 # =======================
 # 데이터 및 모델 준비
 # =======================
-data = pd.DataFrame({
-    'user_id': [0, 1, 1, 2, 3, 3],
-    'item_id': [101, 101, 102, 103, 102, 104],
-    'purchase_count': [1, 1, 2, 1, 1, 1]
-})
+# data = pd.DataFrame({
+#     'user_id': [0, 1, 1, 2, 3, 3],
+#     'item_id': [101, 101, 102, 103, 102, 104],
+#     'purchase_count': [1, 1, 2, 1, 1, 1]
+# })
 
 # 인코딩
 user_enc = LabelEncoder()
